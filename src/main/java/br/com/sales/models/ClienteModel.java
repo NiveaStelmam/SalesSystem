4b8deac91daa4ccede5941be6a2d872453623529
Nivea Stelmam <mand458@hotmail.com>
2024-09-16 22:33:45 -0300

    updated

--- src/main/java/br/com/sales/models/ClienteModel.java
+++ src/main/java/br/com/sales/models/ClienteModel.java
@@ -5,9 +5,17 @@ public class ClienteModel {
     private Integer id;
     private String nome;
 
-    public Integer getId() {
-        return id;
+    public ClienteModel(){}
+    public ClienteModel(String nome){
+        this.nome = nome;
     }
+
+    public ClienteModel(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() { return id;}
 
     public void setId(Integer id) {
         this.id = id;
