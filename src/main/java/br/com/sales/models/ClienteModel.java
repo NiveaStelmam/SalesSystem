6c5e5520689092d1a321b9b1be01ee1678db016a
Nivea Stelmam <mand458@hotmail.com>
2024-09-20 10:59:47 -0300

    client entity refactoring

--- src/main/java/br/com/sales/models/ClienteModel.java
+++ src/main/java/br/com/sales/models/ClienteModel.java
@@ -2,46 +2,34 @@ package br.com.sales.models;
 
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name= "tb_cliente")
 public class ClienteModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
-    private Integer id;
+    private UUID id;
 
     @Column(name = "nome")
     @NotBlank(message = "O campo 'nome' não pode estar em branco ou vazio")
     @Size( min = 3, max = 50)
     private String nome;
 
-    public ClienteModel(){}
+    // ---------------------------------------------
+
     public ClienteModel(String nome){
-        this.nome = nome;
-    }
-
-    public ClienteModel(Integer id, String nome) {
-        this.id = id;
-        this.nome = nome;
-    }
-
-    public Integer getId() { return id;}
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
         this.nome = nome;
     }
 }
--- src/main/java/br/com/sales/repository/ClienteRepository.java
+++ src/main/java/br/com/sales/repository/ClienteRepository.java
@@ -5,15 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 
 @Repository
-public interface ClienteRepository extends JpaRepository<ClienteModel, Integer> {
+public interface ClienteRepository extends JpaRepository<ClienteModel, UUID> {
 
-    // select c from Cliente c where c.nome like : nome (query)
-    List<ClienteModel> findByNomeLike(String nome);  // declaração
+    // select c from Cliente c where c.nome like : nome (query) methods
+    List<ClienteModel> findByNomeLike(String nome);  // declaration
 
-    List<ClienteModel> findByNomeOrId(String nome, Integer id);
+    List<ClienteModel> findByNomeOrId(String nome, UUID id);
 
     boolean existsByNome(String nome);
 
